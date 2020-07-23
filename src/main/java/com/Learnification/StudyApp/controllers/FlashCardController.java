c15dba742f640893749822a37bb2c197baca6713
Marms <maryarmstrong314@gmail.com>
2020-07-23 00:22:33 -0500

    import cleanup

--- src/main/java/com/Learnification/StudyApp/controllers/FlashCardController.java
+++ src/main/java/com/Learnification/StudyApp/controllers/FlashCardController.java
@@ -6,7 +6,6 @@ import com.Learnification.StudyApp.models.data.CardDeckRepository;
 import com.Learnification.StudyApp.models.data.CategoryRepository;
 import com.Learnification.StudyApp.models.data.FlashCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;

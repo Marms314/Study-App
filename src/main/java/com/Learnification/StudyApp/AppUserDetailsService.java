ef4d9d18d2463062e5299c508683995c3dea4213
Marms <maryarmstrong314@gmail.com>
2020-09-09 20:02:04 -0500

    Set up basic authentication connected to the database

---
+++ src/main/java/com/Learnification/StudyApp/AppUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.Learnification.StudyApp;
+
+import com.Learnification.StudyApp.models.AppUserDetails;
+import com.Learnification.StudyApp.models.User;
+import com.Learnification.StudyApp.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(username);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
+
+        return user.map(AppUserDetails::new).get();
+    }
+}
---
+++ src/main/java/com/Learnification/StudyApp/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.Learnification.StudyApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/flashcard/create", "/flashcard/manage").hasRole("ADMIN")
+                .antMatchers("/quiz/create", "/quiz/manage").hasRole("ADMIN")
+                .antMatchers("/category/create", "/category/manage").hasRole("ADMIN")
+                .antMatchers("/user/create", "/user/manage").hasRole("ADMIN")
+                .antMatchers("/").hasAnyRole("USER", "ADMIN")
+                .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
